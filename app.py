d96c6fe50af96f9673925d9c94ac89f80d0c73d3
Jm <21mk406@esisalama.org>
2025-05-16 23:38:36 +0200

    Ajout de CORS pour permettre l'accès de l'application Android

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # très important pour permettre à l'app Android d'accéder
 
 # Variable de contrôle globale
 APP_ACTIVE = True
